1e5e76413dd8de24f6012a870e1a2b5af9c7e342
Abdul Lathif Shaik <74287540+lathifshaik@users.noreply.github.com>
2023-08-23 14:40:25 +0530

    Update tasks.py
    Updated the ccextracter to ccextracter.exe file to work with windows when tested locelly

--- videoapp/tasks.py
+++ videoapp/tasks.py
@@ -25,7 +25,7 @@ def process_video(video_path):
     video_id, _ = os.path.splitext(video_filename)
     # Extract subtitles using ccextractor
     output_file = f"{video_id}.srt"
-    subprocess.run(['videoapp/CCExtractor_win_portable/ccextractor', video_path, '-o', output_file])
+    subprocess.run(['videoapp/CCExtractor_win_portable/ccextractorwinfull.exe', video_path, '-o', output_file])
     
     # Upload video to S3
     s3.Bucket('videoapp-django').upload_file(video_path, f"videos/{video_id}.mp4")
